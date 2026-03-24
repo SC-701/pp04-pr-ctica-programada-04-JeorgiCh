70833372db4d608a690195cf963d9fa61339b2e9
JeorgiCh <jorgech3005@gmail.com>
2026-03-23 19:29:22 -0600

    Practica terminada

---
+++ Vehiculos.WEB/Abstracciones/Modelos/Seguridad/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class UsuarioBase
+{
+    [Required]
+    public string NombreUsuario { get; set; } 
+
+    public string? PasswordHash { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string CorreoElectronico { get; set; } 
+}
+
+public class Usuario : UsuarioBase
+{
+    [Required]
+    public string Password { get; set; } 
+
+    [Required]
+    public string ConfirmarPassword { get; set; } 
+}
